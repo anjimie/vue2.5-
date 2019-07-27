86df160261bfda1c21e68fdfbe5bb8b386bf9ac8
anjimie <anjimie@163.com>
2019-07-28 00:22:49 +0800

    ''

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -1,36 +1,35 @@
 <template>
-    <div class="header">
-        城市选择
-        <router-link to='/'>
-            <div class="iconfont header-back">&#xe624;</div>
-        </router-link>
-    </div>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'CityHeader'
+  name: 'CityHeader'
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/varibles.styl'
-.header
-// 返回没显示：需要加一个定位
-    position:relative
-    overflow :hidden
-    height :$headerHeight
-    line-height :$headerHeight
-    background :$bgColor
-    color:#fff
-    text-align:center
-    font-size :.32rem
+  @import '~styles/varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
     .header-back
-        position :absolute
-        top:0
-        left:0
-        width :.64rem
-        text-align :center
-        font-size:.4rem
-        color:#fff
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
 </style>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,50 +1,71 @@
 <template>
-<!-- ref:获取dom -->
-    <div class="list" ref="wrapper">
-        <!-- better-scroll需要再有一层div -->
-        <div>
-            <div class="area">
-                <div class="title border-topbottom">当前城市</div>
-                <div class="button-list">
-                    <div class="button-wrapper" v-for="item of hot" :key="item.id">
-                        <div class="button">{{item.name}}</div> 
-                    </div>
-                </div>
-            </div>
-            <div class="area">
-                <div class="title border-topbottom">热门城市</div>
-                <div class="button-list">
-                    <div class="button-wrapper">
-                        <div class="button">北京</div> 
-                    </div>
-                </div>
-            </div>
-            <!-- 循环对象，所以第二项是key -->
-            <div class="area" 
-            v-for="(item,key) of cities" 
-            :key="key"
-            :ref="key"
-            >
-                <div class="title border-topbottom">{{key}}</div>
-                <div class="item-list">
-                    <div class="item border-bottom"
-                    v-for="innerItem of item" :key="innerItem.id">{{innerItem.name}}</div>
-                </div>                                           
-            </div>  
-      </div>          
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">{{this.currentCity}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div
+            class="button-wrapper"
+            v-for="item of hot"
+            :key="item.id"
+            @click="handleCityClick(item.name)"
+          >
+            <div class="button">{{item.name}}</div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+      >
+        <div class="title border-topbottom">{{key}}</div>
+        <div class="item-list">
+          <div
+            class="item border-bottom"
+            v-for="innerItem of item"
+            :key="innerItem.id"
+            @click="handleCityClick(innerItem.name)"
+          >
+            {{innerItem.name}}
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import BScroll from 'better-scroll'
+import Bscroll from 'better-scroll'
+import { mapState, mapMutations } from 'vuex'
 export default {
-    name:'CityList',
-    props:{
-        cities:Object,
-        hot:Array,
-        letter:String
+  name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object,
+    letter: String
+  },
+  computed: {
+    ...mapState({
+      currentCity: 'city'
+    })
+  },
+  methods: {
+    handleCityClick (city) {
+      this.changeCity(city)
+      this.$router.push('/')
     },
-    //letter变化时， 侦听器使该letter的相关的值显示
+    ...mapMutations(['changeCity'])
+  },
   watch: {
     letter () {
       if (this.letter) {
@@ -53,56 +74,49 @@ export default {
       }
     }
   },
-    mounted(){
-        this.scroll = new BScroll(this.$refs.wrapper)
-    }
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/varibles.styl'
-.border-topbottom
+  @import '~styles/varibles.styl'
+  .border-topbottom
     &:before
-        border-color:#ccc
-    &after 
-        border-color:#ccc    
-.border-bottom
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
     &:before
-        border-color:#ccc  
-//不能滚动    
-.list
-    overflow :hidden
-    position :absolute
-    top:1.58rem
-    right:0
-    left:0
-    bottom :0
-    // background :red
+      border-color: #ccc
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
     .title
-        line-height :.54rem
-        background #eee
-        padding-left:.2rem
-        color:#666
-        font-size :.24rem
+      line-height: .54rem
+      background: #eee
+      padding-left: .2rem
+      color: #666
+      font-size: .26rem
     .button-list
-        padding:.1rem  .6rem .1rem .1rem
-        overflow :hidden
-        .button-wrapper
-            float: left
-            width :33.33%
-            .button
-                margin :.1rem
-                padidng:.1rem 0
-                text-align :center
-                border:.02rem solid #ccc
-                border-radius:.06rem
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float: left
+        width: 33.33%
+        .button
+          margin: .1rem
+          padding: .1rem 0
+          text-align: center
+          border: .02rem solid #ccc
+          border-radius: .06rem
     .item-list
-        .item   
-            line-height :.76rem
-            // color :#666
-            padding-left:.2rem
-
-
-                
-
+      .item
+        line-height: .76rem
+        padding-left: .2rem
 </style>
